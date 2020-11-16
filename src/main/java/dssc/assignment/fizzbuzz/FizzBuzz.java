d019fe7bc4ffe42de2b2a8862d93c6b122be6870
cecia234 <cecia234@gmail.com>
2020-11-16 17:42:20 +0100

    Added Solution

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,19 @@ package dssc.assignment.fizzbuzz;
 public class FizzBuzz {
 
     public static void main(String... args) {
+       for(int i = 1; i<106;i++){
+            String str="";
+            if(i%3==0)
+                str+="Fizz";
+            if (i%5==0)
+                str+="Buzz";
+            if (i%7==0)
+                str+="Bang";
+            if(str.isEmpty())
+                System.out.println(i);
+            else
+                System.out.println(str);
+        }
         
     }
 
